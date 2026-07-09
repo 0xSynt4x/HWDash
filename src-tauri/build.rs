714e440b9e5f17a43f4556a8f0c40356856661aa
cnb.cUW3OCnuAFA <Ctf1jVYZlOk8EeFsLjVrJI+cnb.cUW3OCnuAFA@noreply.cnb.cool>
2026-07-10 04:35:19 +0800

    修复Windows manifest: 添加Common Controls 6.0依赖和DPI awareness

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,11 +2,17 @@ fn main() {
     tauri_build::build();
 
     // 请求管理员权限 (LibreHardwareMonitor 需要 admin 才能读取 AMD CPU 的温度/电压/功耗/频率传感器)
+    // 同时保留 Common Controls 6.0 / DPI awareness 等 Tauri 默认 manifest 内容
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     if target_os == "windows" {
         let mut res = tauri_winres::WindowsResource::new();
         res.set_manifest(r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*"/>
+    </dependentAssembly>
+  </dependency>
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
       <requestedPrivileges>
@@ -14,6 +20,12 @@ fn main() {
       </requestedPrivileges>
     </security>
   </trustInfo>
+  <application xmlns="urn:schemas-microsoft-com:asm.v3">
+    <windowsSettings>
+      <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/pm</dpiAware>
+    </windowsSettings>
+  </application>
 </assembly>"#);
         if let Err(e) = res.compile() {
             println!("cargo:warning=Failed to compile winres manifest: {}", e);
